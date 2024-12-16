ca4484f777130aff53d067474e000a4ead6871eb
Lovren <95935854+j337y@users.noreply.github.com>
2024-12-16 08:20:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        PodKlasa podklasa = new PodKlasa();
+
+        Console.WriteLine(podklasa.GetString());
+
+        Console.ReadKey();
+    }
+}
+
+public class PodKlasa : NadKlasa
+{
+    public string GetString()
+    {
+        return this.String;
+    }
+}
+
+public class NadKlasa
+{
+    protected string String = "Ja sam zasticeni string!";
+}
+
+
